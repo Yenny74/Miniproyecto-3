ce639983ba4bb6c7af7222d2fd22f43d0e0bfa0b
Yenny74 <120958834+Yenny74@users.noreply.github.com>
2022-12-21 20:16:10 -0500

    Add files via upload

---
+++ ServicioDeSalud/src/main/java/com/mycompany/serviciodesalud/Consultorio.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.serviciodesalud;
+
+/**
+ *
+ * @author Yenny
+ */
+public class Consultorio 
+{
+  String nombreConsultorio;
+  Medicos medicoConsultorio;
+  Servicio servicioConsultorio;
+  
+  public Consultorio()
+ {
+ }
+}
+
+
---
+++ ServicioDeSalud/src/main/java/com/mycompany/serviciodesalud/ServicioDeSalud.java
@@ -0,0 +1,13 @@
+
+
+package com.mycompany.serviciodesalud;
+
+
+public class ServicioDeSalud {
+
+    public static void main(String[] args) 
+    {
+       Entrada ventana = new Entrada();
+       ventana.setVisible(true);
+    }
+}
